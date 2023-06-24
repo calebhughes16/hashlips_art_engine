6fb299b41884a1e0e5b4e2647511bbba2c2688c8
Caleb Hughes <calebhughes16@gmail.com>
2023-06-24 14:20:25 -0400

    INFKTED

--- src/config.js
+++ src/config.js
@@ -27,12 +27,9 @@ const layerConfigurations = [
     growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Faction" },
+      { name: "Item Type" },
+      { name: "Rarity" },
     ],
   },
 ];
